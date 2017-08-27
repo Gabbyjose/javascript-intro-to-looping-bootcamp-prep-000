62ced05a936b9d51c2733acb5b2df5e497cbc681
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-08-27 18:01:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,17 @@
+function forLoop(){
+  var finalStr = '';
+  for(i=1; i< 25; i++){
+    if (i == 1){
+      finalStr += 'I am 1 strange loop. ';
+    }
+    else {
+      finalStr += 'I am ${i} strange loops. ';
+    }
+  }
+  return finalStr;
+}
+
+
 function whileLoop(num){
   while (num > 0){
     console.log(num--);
